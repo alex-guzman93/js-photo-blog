12f1b71635a520d85f50c41002626b0600d4fe37
alexander guzman <Alex.networking93@gmail.com>
2026-01-12 17:49:13 +0100

    aggiunto background img, e le card tramite api

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-//seleiono elemento conteiner output 
-const outputCont = document.getElementById ("container")
+//seleiono elemento di conteiner output 
+const outputCont = document.querySelector(".container-cards")
+    // console.log(outputCont);
+
+// creo riferimento endpoint
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
+
+//creo la chiamata ajax all'endpoint
+
+axios.get(endpoint)
+.then(response => {
+    
+    const dataArray = response.data
+     console.log(dataArray);
+    
+     dataArray.forEach(dataCard => {
+        console.log(dataCard);
+
+        //aggiungiamo una card per ogni forEach loop
+        outputCont.innerHTML +=`<figure class="card">
+                <img src=${dataCard.url} class="imgs" alt="#">
+                <figcaption> ${dataCard.title}</figcaption>
+                
+            </figure>`
+     });
+})
+
+.catch()
+           
