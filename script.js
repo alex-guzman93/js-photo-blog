e36011fd9b29f54d17a8ee14aa981a65a43b9e18
alexander guzman <Alex.networking93@gmail.com>
2026-01-13 17:56:48 +0100

    aggiunto overlay e le funzionalita

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //seleiono elemento di conteiner output 
 const outputCont = document.querySelector(".container-cards")
-    // console.log(outputCont);
+const overlay = document.querySelector(".overlay")
+const onOff = document.querySelector(".on-off")
+// console.log(outputCont);
 
 // creo riferimento endpoint
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
@@ -8,23 +10,32 @@ const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
 //creo la chiamata ajax all'endpoint
 
 axios.get(endpoint)
-.then(response => {
-    
-    const dataArray = response.data
-     console.log(dataArray);
-    
-     dataArray.forEach(dataCard => {
-        console.log(dataCard);
+    .then(response => {
 
-        //aggiungiamo una card per ogni forEach loop
-        outputCont.innerHTML +=`<figure class="card">
+        const dataArray = response.data
+        // console.log(dataArray);
+
+        dataArray.forEach(dataCard => {
+            // console.log(dataCard);
+
+            //aggiungiamo una card per ogni forEach loop
+            outputCont.innerHTML += `<figure class="card">
                 <img class="pin" src="./img/pin.svg" alt="#">
                 <img src=${dataCard.url} class="imgs" alt="#">
                 <figcaption> ${dataCard.title}</figcaption>
-                
             </figure>`
-     });
-})
 
-.catch()
-           
+        });
+        const figures = document.querySelectorAll(".card")
+        console.log(figures);
+        figures.forEach(figure => {
+            figure.addEventListener("click", () => {
+                // console.log(figure);
+                overlay.style.display = "flex"
+            })
+        })
+        onOff.addEventListener("click", () => {
+            overlay.style.display = "none"
+        })
+    })
+    .catch()
